fad4dc77381fa3548858d0fb7f02d457dad672a4
Alain Greppin <agreppin@chilibi.ch>
2024-06-04 14:15:18 +0200

    okcc-run.sh use tcc -run if possible

--- okcc-run.sh
+++ okcc-run.sh
@@ -15,22 +15,50 @@ trap '/usr/bin/rm -f "$tmpc" "$tmpe"' EXIT INT QUIT TERM
 
 # bash 42ShellTester.sh --all --reference ~/src/oksh/oksh ~/src/okcc/okcc-run.sh
 # 42ShellTester --filter path
-SAVED_PATH="${PATH:-}"
+saved_path="${PATH:-}"
 export PATH="/usr/local/bin:/usr/bin:${PATH-}"
+
+# try to find a fast compiler
+_find_cc() {
+  local IFS=":$IFS"
+  local cc x
+  case ${CC-} in
+  '')
+    for x in ${PATH}; do
+      for cc in tcc clang gcc cc c99; do
+        if [ -x "${x}/${cc}" ]; then
+          CC="${x}/${cc}"
+          break 2
+        fi
+      done
+    done
+    ;;
+  esac
+}
 
 if grep -Fq 'compcert' "$topd/okcc"; then
   CC=ccomp
   LCCOMP='-Wl,-znoexecstack'
 elif grep -Fq 'gcov' "$topd/okcc"; then
   lgcov='-lgcov'
+else
+  _find_cc
 fi
 
 "$topd/okcc" "$@" > "$tmpc"
-${CC-cc} -o "$tmpe" "$tmpc" -L"$topd" -lokcc ${lgcov:-} ${LCCOMP-}
+ldflags="-L$topd -L$topd/../lib"
+case ${CC-} in
+*'tcc'*) ;; # use tcc -run
+*) ${CC-cc} -o "$tmpe" "$tmpc" ${ldflags} -lokcc ${lgcov:-} ${LCCOMP-};;
+esac
 
-export PATH="$SAVED_PATH"
+export PATH="$saved_path"
+unset saved_path
 case $# in
 0) ;;
 *) [ -e "$1" ] && shift;;
 esac
-"$tmpe" "$@"
+case ${CC-} in
+*'tcc'*) ${CC} "-run ${ldflags} -lokcc" "$tmpc" -- "$@";;
+*) "$tmpe" "$@";;
+esac
